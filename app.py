5fd603cf345f113036d7f6f46d79b16059a484a7
Kevin-Liu-TW <Kevinliu1990713@gmail.com>
2025-06-04 00:21:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'default-secret')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
