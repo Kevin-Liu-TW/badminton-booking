0e6e5d3ce379c68ac633f41e8545ee862bb7771d
Kevin-Liu-TW <Kevinliu1990713@gmail.com>
2025-06-06 11:15:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-with app.app_context():
-    db.create_all()
-
 migrate = Migrate(app, db)
 
 
