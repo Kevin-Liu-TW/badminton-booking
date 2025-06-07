cf93a5b7ddd6be82536830db21c3e6f2407e8f55
Kevin-Liu-TW <Kevinliu1990713@gmail.com>
2025-06-07 21:09:22 +0800

    Update app.py
    Test config

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 from config import config_map
 
 load_dotenv()
-app_env = os.environ.get("app_env", "development")
-app_config = config_map.get(app_env, config_map["development"])
+app_env = os.environ.get("app_env", "production")
+app_config = config_map.get(app_env, config_map["production"])
 
 from flask import Flask, render_template, request, redirect, url_for, session, flash, abort
 from flask_sqlalchemy import SQLAlchemy
